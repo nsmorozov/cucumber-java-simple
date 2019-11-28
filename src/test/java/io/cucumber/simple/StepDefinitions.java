7b07669e93b2a94b0faf08cade71e651696300ac
Nikolay <nmorozov_of@mail.ru>
2019-11-28 23:58:50 +0600

    added Selenide support and test step to check

--- src/test/java/io/cucumber/simple/StepDefinitions.java
+++ src/test/java/io/cucumber/simple/StepDefinitions.java
@@ -1,13 +1,22 @@
 package io.cucumber.simple;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class StepDefinitions {
 
     @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
+    public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         System.out.println("Works for me");
         belly.eat(cukes);
+    }
+
+    @Then("I open home page")
+    public void iOpenHomePage() {
+        open("https://www.google.com");
+        System.out.println();
     }
 }
